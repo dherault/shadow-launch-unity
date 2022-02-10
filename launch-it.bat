01a0e17d54e73767512e8314cc7f4b240cce8209
David Hérault <dherault@gmail.com>
2022-02-10 17:06:17 +0200

    Update launch-it.bat

--- launch-it.bat
+++ launch-it.bat
@@ -2,8 +2,4 @@ start "" "C:\Program Files\Virtual Desktop Streamer\VirtualDesktop.Streamer.exe"
 start "" "C:\Users\Shadow\AppData\Local\GitHubDesktop\GitHubDesktop.exe"
 start "" "C:\Program Files\JetBrains\JetBrains Rider 2021.2.2\bin\rider64.exe" "C:\Users\Shadow\Documents\GitHub\Phenomenom\Phenomenom.sln"
 
-echo "Press a key"
-
-timeout 5
-
 "C:\Program Files\Virtual Desktop Streamer\VirtualDesktop.Streamer.exe" "C:\Program Files\Unity\Hub\Editor\2020.3.24f1\Editor\Unity.exe" -projectpath "C:\Users\Shadow\Documents\GitHub\Phenomenom -cloudEnvironment"
